e0d3335946a32c72349c7b803be1ee9e340830ab
Nicole Rauch <nicole.m@gmx.de>
2012-08-09 23:23:54 +0200

    Die Code- und Testbase von Ralf Schoch

---
+++ Trivia/GameRunner.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UglyTrivia;
+
+namespace Trivia
+{
+    public class GameRunner
+    {
+
+        private static bool notAWinner;
+
+        public static void main(String[] args)
+        {
+            Game aGame = new Game();
+
+            aGame.add("Chet");
+            aGame.add("Pat");
+            aGame.add("Sue");
+
+            //Random rand = new Random();       Original Code
+            // Creates not random series. Take 4711, 0815, 42, etc. to be time independent
+            Random rand = new Random(4711);     
+
+            do
+            {
+
+                aGame.roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    notAWinner = aGame.wrongAnswer();
+                }
+                else
+                {
+                    notAWinner = aGame.wasCorrectlyAnswered();
+                }
+
+
+
+            } while (notAWinner);
+
+        }
+
+
+    }
+
+}
+
---
+++ TriviaTest/GameRunnerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trivia;
+
+namespace TriviaTest
+{
+    [TestClass]
+    public class GameRunnerTest
+    {
+        private StringBuilder _output;
+        TextWriter textWriter;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _output = new StringBuilder();
+            textWriter = new StringWriter(_output);
+            Console.SetOut(textWriter);
+        }
+
+        [TestMethod]
+        public void Main_Run_CompareWithOriginal()
+        {
+            string expected = File.ReadAllText("master.txt");
+
+            GameRunner.main(null);
+            
+            Assert.AreEqual(expected, _output.ToString());
+        }
+
+        [TestMethod]
+        public void Main_CreateNewMaster_WritesNewMasterTxt()
+        {
+            GameRunner.main(null);
+
+            File.WriteAllText("newMaster.txt", textWriter.ToString());
+        }
+    }
+}
